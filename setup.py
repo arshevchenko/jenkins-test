5e8fd2307dd78bfad192b6821421da82b8c47a41
Artem Shevchenko <shevchenkoaa@sgu.ru>
2017-01-30 15:34:13 +0400

    Test webhook #2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name="gdtesttask",
-    version="1.0.4",
+    version="1.0.2",
     packages=['gdtesttask'],
     description="Simple python project for jenkins task",
     long_description=long_description,
